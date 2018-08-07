72d45ae78cb92bbf8ad72e3edc387e82820b8bcd
amythpaddy <amythpaddy21@gmail.com>
2018-08-07 13:29:40 +0530

    First Stable Build

--- app/src/main/java/com/caragiz_studioz/test/magicodev1/ActivityMain.java
+++ app/src/main/java/com/caragiz_studioz/test/magicodev1/ActivityMain.java
@@ -1,17 +1,254 @@
 package com.caragiz_studioz.test.magicodev1;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.util.TypedValue;
+import android.view.Surface;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ActivityMain extends AppCompatActivity {
+import com.caragiz_studioz.test.magicodev1.env.BorderedText;
+import com.caragiz_studioz.test.magicodev1.env.ImageUtils;
+import com.caragiz_studioz.test.magicodev1.fromOrigin.CameraActivity;
+import com.caragiz_studioz.test.magicodev1.tracking.MultiBoxTracker;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class ActivityMain extends CameraActivity {
 
     private static final int TF_OF_API_INPUT_SIZE = 300;
-    private static final String TF_OD_API_MODEL_FILE = "file:///android_asset/ssd_mobilenet_v1_android_export.pb";
+    private static final String TF_OD_API_MODEL_FILE = "file:///android_asset/graph.pb";
+    private static final String TF_OD_API_LABELS = "file:///android_asset/labels.txt";
 
+    private enum DetectorMode {
+        TF_OD_API, MULTIBOX, YOLO;
+    }
+
+    private static final DetectorMode MODE = DetectorMode.TF_OD_API;
+
+    private static final float MINIMUM_CONFIDENCE = 0.6f;
+    private static final boolean MAINTAIN_ASPECT = MODE == DetectorMode.YOLO;
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+    private static final boolean SAVE_PREVIEW_BITMAP = false;
+    private static final float TEXT_SIZE = 10;
+
+    private Integer sensorOrientation;
+    private Classifier detector;
+
+    private long lastProcessingTimeMs;
+    private Bitmap rgbFrameBitmap = null;
+    private Bitmap croppedBitmap = null;
+    private Bitmap cropCopyBitmap = null;
+
+    private boolean computingDetection = false;
+
+    private long timestamp = 0;
+
+    private Matrix frameToCropTransform;
+    private Matrix cropToFrameTransform;
+
+    private byte[] luminanceCopy;
+    private BorderedText borderedText;
+
+    private OverlayView trackingOverlay;
+    private MultiBoxTracker tracker;
+
+    private TextView displayScreen;
+    private int randNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        displayScreen = findViewById(R.id.display_screen);
+        generateNumber();
     }
+
+    private void generateNumber() {
+        Random r = new Random();
+        randNum = r.nextInt(10) + 1;
+        displayScreen.setText(randNum+"");
+    }
+
+    @Override
+    protected void processImage() {
+        ++timestamp;
+        final long currTimestamp = timestamp;
+        byte[] originalLuminance = getLuminance();
+
+        tracker.onFrame(
+                previewWidth,
+                previewHeight,
+                getLuminanceStride(),
+                sensorOrientation,
+                originalLuminance,
+                timestamp
+        );
+        trackingOverlay.postInvalidate();
+
+        if (computingDetection) {
+            readyForNextImage();
+            return;
+        }
+        computingDetection = true;
+        rgbFrameBitmap.setPixels(getRgbBytes(), 0,
+                previewWidth, 0, 0,
+                previewWidth, previewHeight);
+
+        if (luminanceCopy == null) {
+            luminanceCopy = new byte[originalLuminance.length];
+        }
+        System.arraycopy(originalLuminance, 0, luminanceCopy, 0, originalLuminance.length);
+        readyForNextImage();
+
+        final Canvas canvas = new Canvas(croppedBitmap);
+        canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+        if (SAVE_PREVIEW_BITMAP) {
+            ImageUtils.saveBitmap(croppedBitmap);
+        }
+
+        runInBackground(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Running Detecttion at", currTimestamp + "");
+                final long startTime
+                        = SystemClock.uptimeMillis();
+                final List<Classifier.Recognition> results = detector.recognizeImage(croppedBitmap);
+                lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
+
+                cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+                final Canvas canvas = new Canvas(cropCopyBitmap);
+                final Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setStrokeWidth(2.0f);
+
+                float minimumConfidence = MINIMUM_CONFIDENCE;
+                final List<Classifier.Recognition> mappedRecognintions = new LinkedList<>();
+
+                for (final Classifier.Recognition result : results) {
+                    final RectF location = result.getLocation();
+                    if (location != null && result.getConfidence() >= minimumConfidence) {
+                        canvas.drawRect(location, paint);
+
+                        cropToFrameTransform.mapRect(location);
+                        result.setLocation(location);
+                        mappedRecognintions.add(result);
+
+                    }
+                }
+                tracker.trackResults(mappedRecognintions, luminanceCopy, currTimestamp);
+                trackingOverlay.postInvalidate();
+                checkAns(mappedRecognintions);
+
+                computingDetection = false;
+            }
+        });
+
+    }
+
+    private void checkAns(List<Classifier.Recognition> mappedRecognintions) {
+        int temp = 0;
+        Iterator<Classifier.Recognition> recognitions = mappedRecognintions.iterator();
+        while (recognitions.hasNext()) {
+            String id =recognitions.next().getTitle();
+            switch (id) {
+                case "one":
+                    temp = temp + 1;
+                    break;
+                case "two":
+                    temp = temp + 2;
+                    break;
+                case "five":
+                case "five_alt":
+                    temp += 5;
+                    break;
+                default:
+                    temp += 0;
+            }
+        }
+
+        if (temp == randNum)
+        {
+            displayScreen.setText("YOU ARE RIGHT");
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    generateNumber();
+                }
+            },1500);
+        }
+    }
+
+    @Override
+    protected void onPreviewSizeChosen(Size size, int rotation) {
+        final float textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                TEXT_SIZE, getResources().getDisplayMetrics());
+        borderedText = new BorderedText(textSize);
+        borderedText.setTypeface(Typeface.MONOSPACE);
+
+        int cropSize = TF_OF_API_INPUT_SIZE;
+        try {
+            detector = TensorFlowObjectDetectionAPIModel.create(
+                    getAssets(),
+                    TF_OD_API_MODEL_FILE,
+                    TF_OD_API_LABELS,
+                    TF_OF_API_INPUT_SIZE
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Classifier cannot be initialized", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        previewHeight = size.getHeight();
+        previewWidth = size.getWidth();
+
+        sensorOrientation = rotation - getScreenOrientation();
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888);
+        croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Bitmap.Config.ARGB_8888);
+
+        frameToCropTransform = ImageUtils.getTransformationMatrix(
+                previewWidth, previewHeight,
+                cropSize, cropSize,
+                sensorOrientation, MAINTAIN_ASPECT);
+
+        cropToFrameTransform = new Matrix();
+        frameToCropTransform.invert(cropToFrameTransform);
+
+        trackingOverlay = findViewById(R.id.tracking_overlay);
+        tracker = new MultiBoxTracker(this);
+        trackingOverlay.addCallback(new OverlayView.DrawCallback() {
+            @Override
+            public void drawCallback(Canvas canvas) {
+                tracker.draw(canvas);
+            }
+        });
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.camera_connection_fragment_tracking;
+    }
+
+    @Override
+    protected Size getDesiredPreviewFrameSize() {
+        return DESIRED_PREVIEW_SIZE;
+    }
+
+
 }
---
+++ app/src/main/java/com/caragiz_studioz/test/magicodev1/TensorFlowObjectDetectionAPIModel.java
@@ -0,0 +1,173 @@
+package com.caragiz_studioz.test.magicodev1;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+import android.os.Trace;
+import android.util.Log;
+
+import org.tensorflow.Graph;
+import org.tensorflow.Operation;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Vector;
+
+public class TensorFlowObjectDetectionAPIModel implements Classifier {
+    private static final int MAX_RESULTS = 100;
+    private String inputName;
+    private int inputSize;
+
+    private Vector<String> labels = new Vector<>();
+    private int[] intValues;
+    private byte[] byteValues;
+    private float[] outputLocations;
+    private float[] outputScores;
+    private float[] outputClasses;
+    private float[] outputNumDetections;
+    private String[] outputNames;
+
+    private boolean logStats = false;
+
+    private TensorFlowInferenceInterface inferenceInterface;
+
+    public static Classifier create(final AssetManager assetManager,
+                                    final String modelFilename,
+                                    final String labelFilename,
+                                    final int inputSize) throws IOException {
+        final TensorFlowObjectDetectionAPIModel d =
+                new TensorFlowObjectDetectionAPIModel();
+
+        InputStream lablesInput = null;
+        String actualFilename = labelFilename.split("file:///android_asset/")[1];
+        lablesInput = assetManager.open(actualFilename);
+        BufferedReader br = null;
+        br = new BufferedReader(new InputStreamReader(lablesInput));
+        String line;
+        while ((line = br.readLine()) != null) {
+            d.labels.add(line);
+            Log.i("Label Found ", line);
+        }
+        br.close();
+
+        d.inferenceInterface = new TensorFlowInferenceInterface(assetManager, modelFilename);
+        final Graph g = d.inferenceInterface.graph();
+        d.inputName = "image_tensor";
+
+        final Operation inputOp = g.operation(d.inputName);
+        if (inputOp == null)
+            throw new RuntimeException("Failed to find inputNode '" + d.inputName + "'");
+        d.inputSize = inputSize;
+
+        final Operation outputOp1 = g.operation("detection_scores");
+        if (outputOp1 == null)
+            throw new RuntimeException("Failed to find inputNode '" + d.inputName + "'");
+
+        final Operation outputOp2 = g.operation("detection_boxes");
+        if (outputOp2 == null)
+            throw new RuntimeException("Failed to find node detection_boxes");
+
+        final Operation outputOp3 = g.operation("detection_classes");
+        if (outputOp3 == null)
+            throw new RuntimeException("Failed to find node detection_classes");
+
+        d.outputNames = new String[]{"detection_boxes", "detection_scores", "detection_classes",
+                "num_detections"};
+        d.intValues = new int[d.inputSize * d.inputSize];
+        d.byteValues = new byte[d.inputSize * d.inputSize *3];
+        d.outputScores = new float[MAX_RESULTS];
+        d.outputLocations = new float[MAX_RESULTS * 4];
+        d.outputClasses = new float[MAX_RESULTS];
+        d.outputNumDetections = new float[1];
+
+        return d;
+    }
+
+    private TensorFlowObjectDetectionAPIModel() {
+    }
+
+    @Override
+    public List<Recognition> recognizeImage(Bitmap bitmap) {
+        Trace.beginSection("recognizeImage");
+        Trace.beginSection("preprocessBitmap");
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0
+                , bitmap.getWidth(), bitmap.getHeight());
+        try {
+            for (int i = 0; i < intValues.length; ++i) {
+                byteValues[i * 3 + 2] = (byte) (intValues[i] & 0xFF);
+                byteValues[i * 3 + 1] = (byte) ((intValues[i] >> 8) & 0xFF);
+                byteValues[i * 3 + 0] = (byte) ((intValues[i] >> 16) & 0xFF);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+
+        Trace.endSection();
+
+        Trace.beginSection("feed");
+        inferenceInterface.feed(inputName, byteValues, 1, inputSize, inputSize, 3);
+        Trace.endSection();
+
+        Trace.beginSection("run");
+        inferenceInterface.run(outputNames, logStats);
+        Trace.endSection();
+
+        Trace.beginSection("fetch");
+        outputLocations = new float[MAX_RESULTS * 4];
+        outputScores = new float[MAX_RESULTS];
+        outputClasses = new float[MAX_RESULTS];
+        outputNumDetections = new float[1];
+        inferenceInterface.fetch(outputNames[0], outputLocations);
+        inferenceInterface.fetch(outputNames[1], outputScores);
+        inferenceInterface.fetch(outputNames[2], outputClasses);
+        inferenceInterface.fetch(outputNames[3], outputNumDetections);
+        Trace.endSection();
+
+        final PriorityQueue<Recognition> pq = new PriorityQueue<>(1,
+                new Comparator<Recognition>() {
+                    @Override
+                    public int compare(Recognition o1, Recognition o2) {
+                        return Float.compare(o2.getConfidence(), o1.getConfidence());
+                    }
+                });
+
+        for (int i = 0; i < outputScores.length; ++i) {
+            final RectF detection = new RectF(
+                    outputLocations[4 * i + 1] * inputSize,
+                    outputLocations[4 * i] * inputSize,
+                    outputLocations[4 * i + 3] * inputSize,
+                    outputLocations[4 * i + 2] * inputSize
+            );
+            pq.add(new Recognition("" + i, labels.get((int) outputClasses[i]),
+                    outputScores[i], detection));
+        }
+
+        ArrayList<Recognition> recognitions = new ArrayList<>();
+        for (int i = 0; i < Math.min(pq.size(), MAX_RESULTS); ++i) {
+            recognitions.add(pq.poll());
+        }
+        Trace.endSection();
+        return recognitions;
+    }
+
+    @Override
+    public void enableStatLogging(boolean debug) {
+        this.logStats = debug;
+    }
+
+    @Override
+    public String getStatString() {
+        return inferenceInterface.getStatString();
+    }
+
+    @Override
+    public void close() {
+        inferenceInterface.close();
+    }
+}
